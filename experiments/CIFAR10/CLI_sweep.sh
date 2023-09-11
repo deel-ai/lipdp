858ea4db2acfb73ea3a4547644924d1674ce5aa0
AlgueRythme <louis.bethune@ens-lyon.fr>
2023-09-11 13:06:48 +0200

    certifiable accuracy monitoring

--- experiments/CIFAR10/CLI_sweep.sh
+++
@@ -1,5 +0,0 @@
-for noise in 2.5 3.2 3.5 4. 5.:
-do 
-    python experiments/CIFAR10/main_template.py --cfg.log_wandb="sweep_archi" --cfg.noise_multiplier=$noise --cfg.loss="TauCategoricalCrossentropy"
-    python experiments/CIFAR10/main_template.py --cfg.log_wandb="sweep_archi" --cfg.noise_multiplier=$noise --cfg.loss="KCosineSimilarity"
-done
