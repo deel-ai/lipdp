ecbfb8996e23a063ac279f4e6b124cc47f3be02e
AlgueRythme <louis.bethune@ens-lyon.fr>
2023-09-11 13:07:48 +0200

    certifiable accuracy monitoring

---
+++ experiments/CIFAR10/models.py
@@ -0,0 +1,445 @@
+# -*- coding: utf-8 -*-
+# Copyright IRT Antoine de Saint Exupéry et Université Paul Sabatier Toulouse III - All
+# rights reserved. DEEL is a research program operated by IVADO, IRT Saint Exupéry,
+# CRIAQ and ANITI - https://www.deel.ai/
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import collections
+
+import tensorflow as tf
+
+from deel.lipdp.layers import DP_AddBias
+from deel.lipdp.layers import DP_BoundedInput
+from deel.lipdp.layers import DP_ClipGradient
+from deel.lipdp.layers import DP_Flatten
+from deel.lipdp.layers import DP_GroupSort
+from deel.lipdp.layers import DP_Lambda
+from deel.lipdp.layers import DP_LayerCentering
+from deel.lipdp.layers import DP_Permute
+from deel.lipdp.layers import DP_QuickSpectralDense
+from deel.lipdp.layers import DP_Reshape
+from deel.lipdp.layers import DP_ScaledGlobalL2NormPooling2D
+from deel.lipdp.layers import DP_ScaledL2NormPooling2D
+from deel.lipdp.layers import DP_SpectralConv2D
+from deel.lipdp.layers import DP_SpectralDense
+from deel.lipdp.layers import make_residuals
+from deel.lipdp.model import DP_Model
+from deel.lipdp.model import DP_Sequential
+
+
+def create_MLP_Mixer(dp_parameters, dataset_metadata, cfg, upper_bound):
+    """Creates a MLP-Mixer network.
+
+    The cfg object must contain some information:
+        - cfg.add_biases (bool): DP_AddBias layers after each linear layer.
+        - cfg.layer_centering (bool): DP_LayerCentering layers after each activation.
+        - cfg.skip_connections (bool): skip connections in the MLP-Mixer network.
+        - cfg.num_mixer_layers (int): number of mixer layers.
+        - cfg.patch_size (int): size of the patches.
+        - cfg.hidden_size (int): size of the hidden layer.
+        - cfg.mlp_seq_dim (int): size of the hidden layer in the MLP block.
+        - cfg.mlp_channel_dim (int): size of the hidden layer in the channel block.
+        - cfg.clip_loss_gradient (float): clip the gradient of the loss to this value.
+
+    Args:
+        dp_parameters: parameters for differentially private training
+        dataset_metadata: metadata of the dataset, for privacy accounting
+        cfg: configuration containing information for DP_Sequential and MLP-Mixer
+            hyper-parameters
+        upper_bound (float): maximum norm of the input (clipped if input norm is higher)
+
+    Returns:
+        DP_Sequential: DP MLP-Mixer network
+    """
+    input_shape = dataset_metadata.input_shape
+    layers = [DP_BoundedInput(input_shape=input_shape, upper_bound=upper_bound)]
+
+    layers.append(
+        DP_Lambda(
+            tf.image.extract_patches,
+            arguments=dict(
+                sizes=[1, cfg.patch_size, cfg.patch_size, 1],
+                strides=[1, cfg.patch_size, cfg.patch_size, 1],
+                rates=[1, 1, 1, 1],
+                padding="VALID",
+            ),
+        )
+    )
+
+    # layers.append(DP_SpectralConv2D(filters=hidden_dim, kernel_size=patch_size, use_bias=False, strides=patch_size, padding="same"))
+    seq_len = (input_shape[0] // cfg.patch_size) * (input_shape[1] // cfg.patch_size)
+
+    layers.append(DP_Reshape((seq_len, (cfg.patch_size**2) * input_shape[-1])))
+    layers.append(
+        DP_QuickSpectralDense(
+            units=cfg.hidden_size, use_bias=False, kernel_initializer="identity"
+        )
+    )
+
+    for _ in range(cfg.num_mixer_layers):
+        to_add = [
+            DP_Permute((2, 1)),
+            DP_QuickSpectralDense(
+                units=cfg.mlp_seq_dim, use_bias=False, kernel_initializer="identity"
+            ),
+        ]
+        if cfg.add_biases:
+            to_add.append(DP_AddBias())
+        to_add.append(DP_GroupSort(2))
+        if cfg.layer_centering:
+            to_add.append(DP_LayerCentering())
+        to_add += [
+            DP_QuickSpectralDense(
+                units=seq_len, use_bias=False, kernel_initializer="identity"
+            ),
+            DP_Permute((2, 1)),
+        ]
+
+        if cfg.skip_connections:
+            layers += make_residuals("1-lip-add", to_add)
+        else:
+            layers += to_add
+
+        to_add = [
+            DP_QuickSpectralDense(
+                units=cfg.mlp_channel_dim, use_bias=False, kernel_initializer="identity"
+            ),
+        ]
+        if cfg.add_biases:
+            to_add.append(DP_AddBias())
+        to_add.append(DP_GroupSort(2))
+        if cfg.layer_centering:
+            to_add.append(DP_LayerCentering())
+        to_add.append(
+            DP_QuickSpectralDense(
+                units=cfg.hidden_size, use_bias=False, kernel_initializer="identity"
+            )
+        )
+
+        if cfg.skip_connections:
+            layers += make_residuals("1-lip-add", to_add)
+        else:
+            layers += to_add
+
+    # TO REPLACE ?
+    # layers.append(DP_LayerCentering())
+    layers.append(DP_Flatten())
+    layers.append(
+        DP_QuickSpectralDense(units=10, use_bias=False, kernel_initializer="identity")
+    )
+    if cfg.clip_loss_gradient is not None:
+        layers.append(
+            DP_ClipGradient(cfg.clip_loss_gradient, mode=cfg.dynamic_clipping)
+        )
+
+    model = DP_Model(
+        layers,
+        dp_parameters=dp_parameters,
+        dataset_metadata=dataset_metadata,
+        name="mlp_mixer",
+    )
+
+    model.build(input_shape=(None, *input_shape))
+
+    return model
+
+
+def create_VGG(dp_parameters, dataset_metadata, cfg, upper_bound):
+    depth_conv = dict(VGG5_small=1, VGG5_large=1, VGG8_small=2, VGG8_large=2)
+    init_filters = dict(VGG5_small=32, VGG5_large=64, VGG8_small=32, VGG8_large=64)
+
+    assert cfg.architecture in depth_conv.keys()
+    return VGG_factory(
+        dp_parameters,
+        dataset_metadata,
+        cfg,
+        depth_conv=depth_conv[cfg.architecture],
+        depth_fc=1,
+        init_filters=init_filters[cfg.architecture],
+        upper_bound=upper_bound,
+    )
+
+
+def VGG_factory(
+    dp_parameters,
+    dataset_metadata,
+    cfg,
+    depth_conv,
+    depth_fc,
+    init_filters,
+    upper_bound,
+):
+    """Creates a VGG-like network.
+
+    The cfg object must contain some information:
+        - cfg.add_biases (bool): DP_AddBias layers after each linear layer.
+        - cfg.layer_centering (bool): DP_LayerCentering layers after each activation.
+
+    The VGG network is composed of three blocks of `depth` convolutional layers. A
+    pooling layer is appended to each block. The network ends with `depth_fc + 1`
+    fully-connected layers (the last one is the classification layer). The width of each
+    block is:
+        - filters in convolutional block 1: init_filters
+        - filters in convolutional block 2: init_filters * 2
+        - filters in convolutional block 3: init_filters * 4
+        - units in fully-connected layers: init_filters * 8
+
+    Args:
+        cfg: configuration containing information for DP_Sequential and VGG
+            hyper-parameters
+        depth_conv (int): number of convolutions in the three convolutional blocks.
+            Usually depth_conv=1 or 2.
+        depth_fc (int): number of fully-connected layers before the classification
+            layer. Usually depth_fc=1 or 2.
+        init_filters (int): number of filters in the first convolution block. Usually a
+            power of 2 (e.g. 32, 64 or 128).
+        upper_bound (float): maximum norm of the input (clipped if input norm is higher)
+
+    Returns:
+        DP_Sequential: DP VGG network
+    """
+    layers = []
+    layers.append(DP_BoundedInput(input_shape=(32, 32, 3), upper_bound=upper_bound))
+
+    # Convolutional block 1
+    for _ in range(depth_conv):
+        layers.append(
+            DP_SpectralConv2D(
+                filters=init_filters,
+                kernel_size=5,
+                kernel_initializer="orthogonal",
+                strides=1,
+                use_bias=False,
+            )
+        )
+        layers.append(DP_AddBias(norm_max=1))
+        layers.append(DP_GroupSort(2))
+        layers.append(DP_LayerCentering())
+    layers.append(DP_ScaledL2NormPooling2D(pool_size=2, strides=2))
+
+    # Convolutional block 2
+    for _ in range(depth_conv):
+        layers.append(
+            DP_SpectralConv2D(
+                filters=init_filters * 2,
+                kernel_size=3,
+                kernel_initializer="orthogonal",
+                strides=1,
+                use_bias=False,
+            )
+        )
+        layers.append(DP_AddBias(norm_max=1))
+        layers.append(DP_GroupSort(2))
+        layers.append(DP_LayerCentering())
+    layers.append(DP_ScaledL2NormPooling2D(pool_size=2, strides=2))
+
+    # Convolutional block 3
+    for _ in range(depth_conv):
+        layers.append(
+            DP_SpectralConv2D(
+                filters=init_filters * 4,
+                kernel_size=3,
+                kernel_initializer="orthogonal",
+                strides=1,
+                use_bias=False,
+            )
+        )
+        layers.append(DP_AddBias(norm_max=1))
+        layers.append(DP_GroupSort(2))
+        layers.append(DP_LayerCentering())
+    layers.append(DP_ScaledGlobalL2NormPooling2D())
+
+    # Fully connected layers
+    for _ in range(depth_fc):
+        layers.append(
+            DP_SpectralDense(
+                init_filters * 8, use_bias=False, kernel_initializer="orthogonal"
+            )
+        )
+        layers.append(DP_AddBias(norm_max=1))
+        layers.append(DP_GroupSort(2))
+        layers.append(DP_LayerCentering())
+
+    layers.append(DP_SpectralDense(10, use_bias=False, kernel_initializer="orthogonal"))
+    layers.append(DP_AddBias(norm_max=1))
+    layers.append(DP_ClipGradient(cfg.clip_loss_gradient, mode=cfg.dynamic_clipping))
+
+    # Remove DP_AddBias and DP_LayerCentering layers if required
+    if cfg.add_biases is False:
+        layers = [layer for layer in layers if not isinstance(layer, DP_AddBias)]
+    if cfg.layer_centering is False:
+        layers = [layer for layer in layers if not isinstance(layer, DP_LayerCentering)]
+
+    model = DP_Sequential(
+        layers,
+        dp_parameters=dp_parameters,
+        dataset_metadata=dataset_metadata,
+    )
+    return model
+
+
+# -------------------------------------------------------------------------
+# ResNet
+# -------------------------------------------------------------------------
+ModelParams = collections.namedtuple("ModelParams", ["repetitions", "init_filters"])
+
+RESNET_MODELS_DICT = {
+    "resnet6a_small": ModelParams((2,), 32),
+    "resnet6a_large": ModelParams((2,), 64),
+    "resnet6b_small": ModelParams(
+        (
+            1,
+            1,
+        ),
+        32,
+    ),
+    "resnet6b_large": ModelParams(
+        (
+            1,
+            1,
+        ),
+        32,
+    ),
+    "resnet8_small": ModelParams((1, 1, 1), 32),
+    "resnet8_large": ModelParams((1, 1, 1), 64),
+    "resnet10_small": ModelParams((2, 2), 32),
+    "resnet10_large": ModelParams((2, 2), 64),
+}
+
+
+# Helper function
+def handle_block_names(stage, block):
+    name_base = "stage{}_unit{}_".format(stage + 1, block + 1)
+    conv_name = name_base + "spconv"
+    lc_name = name_base + "lc"
+    gs_name = name_base + "gs"
+    pool_name = name_base + "pool"
+    return conv_name, lc_name, gs_name, pool_name
+
+
+# Residual block
+def residual_conv_block(filters, stage, block):
+    # get params and names of layers
+    conv_name, lc_name, gs_name, pool_name = handle_block_names(stage, block)
+
+    layers = []
+    to_add = []
+
+    # first block: pool (except block 0) + additional conv (-> filters)
+    if block == 0:
+        if stage != 0:
+            layers += [
+                DP_ScaledL2NormPooling2D(pool_size=2, strides=2, name=pool_name + "1")
+            ]
+
+        layers += [
+            DP_SpectralConv2D(
+                filters=filters,
+                kernel_size=(1, 1),
+                kernel_initializer="orthogonal",
+                strides=(1, 1),
+                use_bias=False,
+                name=conv_name + "0",
+            )
+        ]
+
+    # continue with convolution layers
+    to_add += [
+        DP_LayerCentering(name=lc_name + "1"),
+        DP_GroupSort(2, name=gs_name + "1"),
+        DP_SpectralConv2D(
+            filters=filters,
+            kernel_size=(3, 3),
+            kernel_initializer="orthogonal",
+            strides=(1, 1),
+            use_bias=False,
+            name=conv_name + "1",
+        ),
+        DP_LayerCentering(name=lc_name + "2"),
+        DP_GroupSort(2, name=gs_name + "2"),
+        DP_SpectralConv2D(
+            filters=filters,
+            kernel_size=(3, 3),
+            kernel_initializer="orthogonal",
+            strides=(1, 1),
+            use_bias=False,
+            name=conv_name + "2",
+        ),
+    ]
+
+    # add residual connection
+    layers += make_residuals("1-lip-add", to_add)
+    return layers
+
+
+# ResNet Builder
+def create_ResNet(dp_parameters, dataset_metadata, cfg, upper_bound):
+    model_params = RESNET_MODELS_DICT[cfg.architecture]
+
+    # CIFAR10
+    classes = 10
+    input_shape = (32, 32, 3)
+    layers = [
+        DP_BoundedInput(input_shape=input_shape, name="data", upper_bound=upper_bound)
+    ]
+
+    # get parameters for model layers
+    init_filters = model_params.init_filters
+
+    # resnet bottom
+    layers += [
+        # set stride 2 in pooling instead of conv
+        DP_SpectralConv2D(
+            filters=init_filters,
+            kernel_size=(3, 3),
+            kernel_initializer="orthogonal",
+            strides=1,
+            use_bias=False,
+            name="conv0",
+        ),
+        DP_ScaledL2NormPooling2D(pool_size=2, strides=2, name="pool0"),
+        DP_LayerCentering(name="lc0"),
+        DP_GroupSort(2, name="gs0"),
+    ]
+
+    # resnet body
+    for stage, rep in enumerate(model_params.repetitions):
+        for block in range(rep):
+            filters = init_filters * (2**stage)
+            layers += residual_conv_block(filters, stage, block)
+
+    # resnet top
+    layers += [
+        DP_ScaledGlobalL2NormPooling2D(name="globalpool1"),
+        DP_SpectralDense(classes, use_bias=False, name="fc1"),
+        DP_ClipGradient(
+            cfg.clip_loss_gradient, mode=cfg.dynamic_clipping, name="clipgrad"
+        ),
+    ]
+
+    model = DP_Model(
+        layers,
+        cfg=cfg,
+        dp_parameters=dp_parameters,
+        dataset_metadata=dataset_metadata,
+        noisify_strategy=cfg.noisify_strategy,
+        name=cfg.architecture,
+    )
+    model.build(input_shape=(None, *input_shape))
+    return model
---
+++ experiments/MNIST/main.py
@@ -0,0 +1,257 @@
+# -*- coding: utf-8 -*-
+# Copyright IRT Antoine de Saint Exupéry et Université Paul Sabatier Toulouse III - All
+# rights reserved. DEEL is a research program operated by IVADO, IRT Saint Exupéry,
+# CRIAQ and ANITI - https://www.deel.ai/
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import os
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from absl import app
+from ml_collections import config_dict
+from ml_collections import config_flags
+
+import wandb
+from deel.lipdp.dynamic import AdaptiveQuantileClipping
+from deel.lipdp.layers import DP_AddBias
+from deel.lipdp.layers import DP_BoundedInput
+from deel.lipdp.layers import DP_ClipGradient
+from deel.lipdp.layers import DP_Flatten
+from deel.lipdp.layers import DP_GroupSort
+from deel.lipdp.layers import DP_LayerCentering
+from deel.lipdp.layers import DP_ScaledL2NormPooling2D
+from deel.lipdp.layers import DP_SpectralConv2D
+from deel.lipdp.layers import DP_SpectralDense
+from deel.lipdp.losses import *
+from deel.lipdp.model import DP_Accountant
+from deel.lipdp.model import DP_Sequential
+from deel.lipdp.model import DPParameters
+from deel.lipdp.pipeline import bound_normalize
+from deel.lipdp.pipeline import default_delta_value
+from deel.lipdp.pipeline import load_and_prepare_images_data
+from deel.lipdp.sensitivity import get_max_epochs
+from experiments.wandb_utils import init_wandb
+from experiments.wandb_utils import run_with_wandb
+from wandb.keras import WandbCallback
+
+
+def default_cfg_mnist():
+    cfg = config_dict.ConfigDict()
+    cfg.add_biases = True
+    cfg.batch_size = 2_000
+    cfg.clip_loss_gradient = None  # not required for dynamic clipping.
+    cfg.dynamic_clipping = "quantiles"  # can be "fixed", "laplace", "quantiles". "fixed" requires a clipping value.
+    cfg.dynamic_clipping_quantiles = (
+        0.9  # crop to 90% of the distribution of gradient norm.
+    )
+    cfg.epsilon_max = 3.0
+    cfg.input_clipping = 0.7
+    cfg.learning_rate = 5e-3
+    cfg.loss = "TauCategoricalCrossentropy"
+    cfg.log_wandb = "disabled"
+    cfg.noise_multiplier = 1.5
+    cfg.noisify_strategy = "local"
+    cfg.optimizer = "Adam"
+    cfg.opt_iterations = None
+    cfg.save = False
+    cfg.save_folder = os.getcwd()
+    cfg.sweep_yaml_config = ""
+    cfg.sweep_id = ""
+    cfg.tau = 32.0
+    return cfg
+
+
+cfg = default_cfg_mnist()
+_CONFIG = config_flags.DEFINE_config_dict("cfg", cfg)
+
+
+def create_ConvNet(dp_parameters, dataset_metadata, cfg, upper_bound):
+    norm_max = 1.0
+    all_layers = [
+        DP_BoundedInput(input_shape=(28, 28, 1), upper_bound=upper_bound),
+        DP_SpectralConv2D(
+            filters=16,
+            kernel_size=3,
+            kernel_initializer="orthogonal",
+            strides=1,
+            use_bias=False,
+        ),
+        DP_AddBias(norm_max=norm_max),
+        DP_GroupSort(2),
+        DP_ScaledL2NormPooling2D(pool_size=2, strides=2),
+        DP_LayerCentering(),
+        DP_SpectralConv2D(
+            filters=32,
+            kernel_size=3,
+            kernel_initializer="orthogonal",
+            strides=1,
+            use_bias=False,
+        ),
+        DP_AddBias(norm_max=norm_max),
+        DP_GroupSort(2),
+        DP_ScaledL2NormPooling2D(pool_size=2, strides=2),
+        DP_LayerCentering(),
+        DP_Flatten(),
+        DP_SpectralDense(1024, use_bias=False, kernel_initializer="orthogonal"),
+        DP_AddBias(norm_max=norm_max),
+        DP_SpectralDense(10, use_bias=False, kernel_initializer="orthogonal"),
+        DP_AddBias(norm_max=norm_max),
+        DP_ClipGradient(
+            clip_value=cfg.clip_loss_gradient,
+            mode="dynamic",
+        ),
+    ]
+    if not cfg.add_biases:
+        all_layers = [
+            layer for layer in all_layers if not isinstance(layer, DP_AddBias)
+        ]
+    model = DP_Sequential(
+        all_layers,
+        dp_parameters=dp_parameters,
+        dataset_metadata=dataset_metadata,
+    )
+    return model
+
+
+def compile_model(model, cfg):
+    # Choice of optimizer
+    if cfg.optimizer == "SGD":
+        optimizer = tf.keras.optimizers.SGD(learning_rate=cfg.learning_rate)
+    elif cfg.optimizer == "Adam":
+        optimizer = tf.keras.optimizers.Adam(learning_rate=cfg.learning_rate)
+    else:
+        print("Illegal optimizer argument : ", cfg.optimizer)
+
+    # Choice of loss function
+    if cfg.loss == "MulticlassHKR":
+        if cfg.optimizer == "SGD":
+            cfg.learning_rate = cfg.learning_rate / cfg.alpha
+        loss = DP_MulticlassHKR(
+            alpha=50.0,
+            min_margin=0.5,
+            reduction=tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE,
+        )
+    elif cfg.loss == "MulticlassHinge":
+        loss = DP_MulticlassHinge(
+            min_margin=0.5,
+            reduction=tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE,
+        )
+    elif cfg.loss == "MulticlassKR":
+        loss = DP_MulticlassKR(reduction=tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE)
+    elif cfg.loss == "TauCategoricalCrossentropy":
+        loss = DP_TauCategoricalCrossentropy(
+            cfg.tau, reduction=tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE
+        )
+    elif cfg.loss == "KCosineSimilarity":
+        loss = DP_KCosineSimilarity(
+            0.99, reduction=tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE
+        )
+    elif cfg.loss == "MAE":
+        loss = DP_MeanAbsoluteError(
+            reduction=tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE
+        )
+    else:
+        raise ValueError(f"Illegal loss argument {cfg.loss}")
+
+    # Compile model
+    model.compile(
+        # decreasing alpha and increasing min_margin improve robustness (at the cost of accuracy)
+        # note also in the case of lipschitz networks, more robustness require more parameters.
+        loss=loss,
+        optimizer=optimizer,
+        metrics=["accuracy"],
+    )
+    return model
+
+
+def train():
+    init_wandb(cfg=cfg, project="MNIST_ClipLess_SGD")
+
+    ds_train, ds_test, dataset_metadata = load_and_prepare_images_data(
+        "mnist",
+        cfg.batch_size,
+        colorspace="RGB",
+        drop_remainder=True,
+        bound_fct=bound_normalize(),
+    )
+
+    model = create_ConvNet(
+        DPParameters(
+            noisify_strategy=cfg.noisify_strategy,
+            noise_multiplier=cfg.noise_multiplier,
+            delta=default_delta_value(dataset_metadata),
+        ),
+        dataset_metadata,
+        cfg,
+        upper_bound=dataset_metadata.max_norm,
+    )
+
+    model = compile_model(model, cfg)
+    model.summary()
+
+    num_epochs = get_max_epochs(cfg.epsilon_max, model)
+
+    adaptive = AdaptiveQuantileClipping(
+        ds_train=ds_train,
+        patience=1,
+        noise_multiplier=cfg.noise_multiplier * 5,  # more noisy.
+        quantile=cfg.dynamic_clipping_quantiles,
+        learning_rate=1.0,
+    )
+    adaptive.set_model(model)
+    callbacks = [
+        WandbCallback(save_model=False, monitor="val_accuracy"),
+        DP_Accountant(),
+        adaptive,
+    ]
+
+    hist = model.fit(
+        ds_train,
+        epochs=num_epochs,
+        validation_data=ds_test,
+        batch_size=cfg.batch_size,
+        callbacks=callbacks,
+    )
+
+    wandb.log(
+        {
+            "Accuracies": wandb.plot.line_series(
+                xs=[
+                    np.linspace(0, num_epochs, num_epochs + 1),
+                    np.linspace(0, num_epochs, num_epochs + 1),
+                ],
+                ys=[hist.history["accuracy"], hist.history["val_accuracy"]],
+                keys=["Train Accuracy", "Test Accuracy"],
+                title="Train/Test Accuracy",
+                xname="num_epochs",
+            )
+        }
+    )
+    if cfg.save:
+        model.save(f"{cfg.save_folder}/{cfg.model_name}.h5")
+
+
+def main(_):
+    run_with_wandb(cfg=cfg, train_function=train, project="ICLR_MNIST_acc")
+
+
+if __name__ == "__main__":
+    app.run(main)
